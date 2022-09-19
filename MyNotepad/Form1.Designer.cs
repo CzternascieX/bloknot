0d52f11f110b1e819d73e824f78aa4e16d1392c5
CzternascieX <nikitka100204@gmail.com>
2022-09-20 00:36:09 +0500

    зеленая форма и нет кнопки закрыть

--- MyNotepad/Form1.Designer.cs
+++ MyNotepad/Form1.Designer.cs
@@ -29,27 +29,14 @@ namespace MyNotepad
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(243, 129);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(141, 53);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Закрыть";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
             this.ClientSize = new System.Drawing.Size(646, 304);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Блокнот";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -58,8 +45,6 @@ namespace MyNotepad
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
 
